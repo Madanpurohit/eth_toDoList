2d1a7efe9d1ee4ffa0b97f9ac77001099ee17b39
Madan Purohit <mspurohit09@gmail.com>
2020-11-26 10:01:47 +0530

    writing full smart contract for todolist

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.5.0;
 contract ToDoList
 {
-    uint taslCount=0;
+    uint public taskCount=0;
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint=>Task) public tasks;
+    function CreateTask(string memory _content) public{
+        taskCount++;
+        tasks[taskCount]=Task(taskCount,_content,false);
+    }
+    constructor() public{
+        CreateTask("Hi this is madan rajpurohit");
+    }
 }
